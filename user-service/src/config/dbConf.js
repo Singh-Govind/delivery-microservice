6b3f0c1ab10f67de237000f25aafb0f2472bb145
Govind <govindkmr141@gmail.com>
2024-09-28 21:59:12 +0530

    changed db name to user-service

--- user-service/src/config/dbConf.js
+++ user-service/src/config/dbConf.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connection = () => {
-    return mongoose.connect("mongodb://localhost:27017/user");
+    return mongoose.connect("mongodb://localhost:27017/user-service");
 }
 
 module.exports = connection;
