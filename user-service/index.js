9777cd1dcfdc95a862be111a0a5dda75ecb1bc29
Govind <govindkmr141@gmail.com>
2024-09-28 22:08:10 +0530

    changed middleware point from user to users

--- user-service/index.js
+++ user-service/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/user", userRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.json({msg: "yes its working!"});
